b87c84f555bb43424c7a6f06adb47de7eb22623e
edgar-mercado <edgar.mercado@mulesoft.com>
2020-02-23 11:57:44 -0600

    fix

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, request, jsonify
-from flask.logging import create_logger
 import logging
 
-import pandas as pd
-from sklearn.externals import joblib
-from sklearn.preprocessing import StandardScaler
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
 
 app = Flask(__name__)
 LOG = create_logger(app)
