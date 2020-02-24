950855a43a9dd0cebe76fd678f4549785fd9174f
edgar-mercado <edgar.mercado@mulesoft.com>
2020-02-24 16:46:53 -0600

    fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask.logging import create_logger
 from flask import render_template
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
