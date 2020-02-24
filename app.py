3452896180fcaedf6b95a8edff8c21eeae1969ee
edgar-mercado <edgar.mercado@mulesoft.com>
2020-02-24 16:29:00 -0600

    fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask.logging import create_logger
 from flask import render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
@@ -15,7 +15,7 @@ def home():
     Root endpoint
     """
     LOG.info("Request made to /")
-    return render_template('static/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
